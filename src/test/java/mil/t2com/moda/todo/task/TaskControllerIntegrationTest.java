95a7815d43b52772731cf80df223c74689375a41
Joshua Reece <joshua.t.reece.mil@army.mil>
2026-03-16 13:23:21 -0500

    git update

--- src/test/java/mil/t2com/moda/todo/task/TaskControllerIntegrationTest.java
+++ src/test/java/mil/t2com/moda/todo/task/TaskControllerIntegrationTest.java
@@ -1,69 +1,92 @@
 package mil.t2com.moda.todo.task;
 
-//@SpringBootTest
-//@Transactional
-//@AutoConfigureMockMvc
+import jakarta.transaction.Transactional;
+import mil.t2com.moda.todo.category.Category;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest // full context
+@Transactional // don't want to fill database
+@AutoConfigureMockMvc // autoconfigure mock
 public class TaskControllerIntegrationTest {
 
-//    @Autowired
-//    private MockMvc mockMvc;
-//
-//    @Autowired
-//    private ObjectMapper objectMapper;
-//
-//    @Autowired
-//    private TaskService taskService;
-//
-//    // Setup test objects
-//
-//    @BeforeEach
-//    void setUp() {
-//        learnTdd = new Task( "Learn TDD", "research TDD", false, started);
-//    }
-//
-//    @Test
-//    public void shouldCreateTask() throws Exception {
-//        String learnTddJson = objectMapper.writeValueAsString(learnTdd);
-//
-//        MvcResult savedTask = mockMvc.perform(MockMvcRequestBuilders
-//                        .post("/api/v1/task")
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(learnTddJson))
-//                .andReturn();
-//        String expectType = savedTask.getRequest().getContentType();
-//        Task expectedTask = objectMapper.readValue(savedTask.getResponse().getContentAsString(), Task.class);
-//
-//        assertEquals(expectType, "application/json");
-//        assertEquals(expectedTask.getTitle(), learnTddJson.getTitle());
-//        assertEquals(expectedTask.getCategory().getLabel(), learnTddJson.getCategory().getLabel());
-//    }
-//
-//    @Test
-//    public void shouldGetAllTasks() throws Exception {
-//        taskRepository.save(firstTask);
-//        taskRepository.save(secondTask);
-//
-//        mockMvc.perform(get("/api/v1/task"))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.*", hasSize(2)))
-//                //.andExpect(jsonPath("$[0].id").value(1L))
-//                .andExpect(jsonPath("$[0].title").value("Learn Tdd"))
-//                .andExpect(jsonPath("$[1].title").value("Practice Tdd"))
-//                //.andExpect(jsonPath("$[0].category.id").value(1L))
-//                .andExpect(jsonPath("$[0].category.label").value("normal"))
-//                .andExpect(jsonPath("$[1].cetegory.label").value("important"));
-//                //.andExpect(jsonPath("$[1].id").value(2L))
-//    }
-//
-//    @Test
-//    public void shouldGetTaskById() throws Exception {
-//        Task savedTask = taskRepository.save(learnTdd);
-//
-//        mockMvc.perform(MockMvcRequestBuilders.get("/api/v1/task/" + savedTask.getId()))
-//                .andExpect(status().isOk())
-//                //.andExpect(jsonPath("$.id").value(1L))
-//                .andExpect(jsonPath("$.title").value(""))
-//                //.andExpect(jsonPath("$.category.id").value(1L))
-//                .andExpect(jsonPath("$.category.label").value("not started"));
-//    }
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private TaskService taskService;
+
+    // Setup test objects
+    Task learnTdd;
+    Category started;
+
+    @BeforeEach
+    void setUp() {
+        started = new Category("started");
+        learnTdd = new Task( "Learn TDD", "research TDD", false, started);
+    }
+
+    @Test
+    public void shouldCreateTask() throws Exception {
+        String learnTddJson = objectMapper.writeValueAsString(learnTdd);
+
+        MvcResult savedTask = mockMvc.perform(MockMvcRequestBuilders
+                        .post("/api/v1/task")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(learnTddJson))
+                .andReturn();
+        String expectedType = savedTask.getRequest().getContentType();
+        Task expectedTask = objectMapper.readValue(savedTask.getResponse().getContentAsString(), Task.class);
+
+        assertEquals(expectedType, "application/json");
+        assertEquals(expectedTask.getTitle(), learnTdd.getTitle()); // Removed "Json" from "learnTdd.getTitle()" to make this work
+        assertEquals(expectedTask.getCategory().getLabel(), learnTdd.getCategory().getLabel()); // Removed "Json" from "learnTdd.getTitle()" to make this work
+    }
+
+    @Test
+    public void shouldGetAllTasks() throws Exception {
+        taskService.saveTask(new Task( "First Task", "new task", false, started));
+        taskService.saveTask(new Task( "Second Task", "another task", false, started));
+
+        // ASSERT
+        mockMvc.perform(get("/api/v1/task"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.*", hasSize(2)))
+                //.andExpect(jsonPath("$[0].id").value(1L))
+                .andExpect(jsonPath("$[0].title").value("First Task"))
+                .andExpect(jsonPath("$[1].title").value("Second Task"))
+                //.andExpect(jsonPath("$[0].category.id").value(1L))
+                .andExpect(jsonPath("$[0].category.label").value("started"))
+                .andExpect(jsonPath("$[1].category.label").value("started"));
+                //.andExpect(jsonPath("$[1].id").value(2L))
+    }
+
+    @Test
+    public void shouldGetTaskById() throws Exception {
+        Task savedTask = taskService.saveTask(new Task("blank task", "no description", false, new Category("failed")));
+
+        mockMvc.perform(get("/api/v1/task/" + savedTask.getId()))
+                .andExpect(status().isOk())
+                //.andExpect(jsonPath("$.id").value(1L))
+                .andExpect(jsonPath("$.title").value(""))
+                //.andExpect(jsonPath("$.category.id").value(1L))
+                .andExpect(jsonPath("$.category.label").value("not started"));
+    }
 }
